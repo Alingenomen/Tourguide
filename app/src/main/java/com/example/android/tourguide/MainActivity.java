f8854c175c9321a9d4fdb15599f597a1add9fe9a
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-03-30 09:52:46 +0200

    Properly encapsulated the MainActivity and added comments for readability

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.android.tourguide;
 
-import android.support.design.widget.NavigationView;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Global variables
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     private int tabHeaderImages[] = new int[] {R.drawable.ic_googlemaps, R.drawable.ic_transport_off,
             R.drawable.ic_dinner_off, R.drawable.ic_nature_off, R.drawable.ic_sports_off};
 
@@ -24,16 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.locationPager);
+        // Initialise the ViewPager
+        initialiseViewPager();
 
+        // Initialise the TabLayout to navigate between fragments
+        initialiseTabLayout();
+    }
+
+    // This method initialises the ViewPager
+    private void initialiseViewPager(){
+        viewPager = findViewById(R.id.locationPager);
         SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
-
         viewPager.setAdapter(adapter);
+    }
 
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+    // This method initialises the TabLayout and
+    // assigns icons to the headers
+    private void initialiseTabLayout(){
+        tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
-
         // configure icons
         for (int i = 0; i < tabHeaderImages.length; i++) {
             tabLayout.getTabAt(i).setIcon(tabHeaderImages[i]);
